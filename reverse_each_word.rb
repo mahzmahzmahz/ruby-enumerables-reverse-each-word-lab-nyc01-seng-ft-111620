8f17ebef7a768b93b7874a317c6757b2888638b9
Alex Marz <alexhmarz@gmail.com>
2020-11-04 22:05:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 
 def reverse_each_word(string)
-new_string = []
- string = string.split(" ")
- string.collect {|element| element.reverse!}
+ new_string = string.split(" ")
+ new_string.collect {|element| element.reverse!}
  return new_string.join(" ")
  
 end
