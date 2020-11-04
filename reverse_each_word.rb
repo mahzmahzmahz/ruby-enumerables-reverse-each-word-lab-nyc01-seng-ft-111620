1dfd88120b6c556ca552086e5297d93e4d91f41b
Alex Marz <alexhmarz@gmail.com>
2020-11-04 21:55:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   reversed_string = []
-  string.each do |element|
-    reversed_string << element.reverse
+    reversed_string << string.reverse
   end
   reversed_string
 end
